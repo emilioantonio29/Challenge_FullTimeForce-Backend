7bb716f3cdee90d28d404cd7cbb996be92729807
emilioantonio29 <emilio_git@outlook.com>
2023-11-28 18:41:24 -0300

    dotenv installed | .env implemented

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
+
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+
+  console.log(`Application is running on: http://localhost:${port}`);
+
 }
 bootstrap();
